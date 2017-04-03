9f51e82e414cc534ab7c778009c58e8e205d87f7
amarquand <amarquand@gmail.com>
2017-04-03 14:53:08 +0200

    added wishart prior to BLR
    added wishart prior to BLR

--- blr_gibbs.m
+++ blr_gibbs.m
@@ -38,23 +38,37 @@ tic; % start timer
 [N,D] = size(X);  
 S = size(Y,2);
 
-% polynomial basis expansion for trend coefficients
-Phi = zeros(size(X,1),size(X,2)*opt.DimPoly); colid = 1:size(X,2);
-for d = 1:opt.DimPoly
-    Phi(:,colid) = X.^d; colid = colid + size(X,2);
-end
-DPhi = size(Phi,2);
+% % polynomial basis expansion for trend coefficients
+% Phi = zeros(size(X,1),size(X,2)*opt.DimPoly); colid = 1:size(X,2);
+% for d = 1:opt.DimPoly
+%     Phi(:,colid) = X.^d; colid = colid + size(X,2);
+% end
+% DPhi = size(Phi,2);
 
 % initialise top-level priors
 an_prior         = opt.PriorParam{1};        % noise
-bn_prior         = opt.PriorParam{2};     
-aa_prior         = opt.PriorParam{3};        % precisions for the weights
-ba_prior         = opt.PriorParam{4}; 
+bn_prior         = opt.PriorParam{2};   
+if size(opt.PriorParam{4},1) > 1
+    disp('using Wishart prior for the weights')
+    n_prior = opt.PriorParam{3}; 
+    P_prior = opt.PriorParam{4};
+    Lmask   = tril(true(size(P_prior)));
+    use_wishart = true;
+else
+    disp('using factorised ARD prior for weights');
+    aa_prior = opt.PriorParam{3};        % precisions for the weights
+    ba_prior = opt.PriorParam{4}; 
+    use_wishart = false;
+end
 
 % initial posterior values
 Theta = opt.X0_Theta;
 beta  = Theta(1);
-alpha = Theta(2:end);
+if use_wishart
+    lambdavec = Theta(2:end);
+else
+    alpha = Theta(2:end);
+end
 
 % initialize posteriors
 Theta_all = zeros(size(Theta,1), opt.nGibbsIter);
@@ -92,15 +106,22 @@ for g = 1:opt.nGibbsIter
     end
        
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % sample W
+    % sample weights (W)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    iSigma = diag(alpha);
+    if use_wishart
+        Ltril        = zeros(D);
+        Ltril(Lmask) = lambdavec;
+        Ldiag        = diag(Ltril);
+        Lambda       = Ltril + Ltril' - diag(Ldiag);
+    else
+        Lambda = diag(alpha);
+    end
     
-    %Prec_w_post = beta*XX + iSigma;
+    %Prec_w_post = beta*XX + Lambda;
     %C = beta*Prec_w_post\X';
     %cholC = chol(inv(Prec_w_post));
     
-    Sigma_w_post = inv(beta*XX + iSigma);
+    Sigma_w_post = inv(beta*XX + Lambda);
     C = beta*Sigma_w_post*X';
     cholC = chol(Sigma_w_post);
     
@@ -114,28 +135,37 @@ for g = 1:opt.nGibbsIter
     %W_all(:,g) = W(:);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % sample beta
+    % sample noise precision (beta)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     R  = Y-X*W;
-    bn = bn_prior + 0.5*trace(R'*R);
-    %tmp = (Y(:,s) - X*W(:,s));
-    %bn = bn_prior + 0.5*tmp'*tmp;
+    %bn = bn_prior + 0.5*trace(R'*R);
+    bn = bn_prior + 0.5*sum(sum(R'.*R',2));
     an = an_prior + 0.5*S*N;
     
     % Draw from gamma
     beta = gamrnd(an,1/bn);
     
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % sample alpha
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    aa = aa_prior + 0.5*S;
-    ba = ba_prior + 0.5*sum(W.*W,2);
-    for d = 1:D
-        alpha(d) = gamrnd(aa,1/ba(d));
-    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % sample weight precision
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if use_wishart 
+        n = n_prior + S;
+        P = P_prior + W*W';
         
-    % update Theta    
-    Theta_all(:,g)  = [beta; alpha];
+        Lambda         = wishrnd(inv(P),n);
+        Ld             = tril(Lambda);
+        lambdavec      = Ld(Lmask);
+        Theta_all(:,g) = [beta; lambdavec];
+    else
+        aa = aa_prior + 0.5*S;
+        ba = ba_prior + 0.5*sum(W.*W,2);
+        for d = 1:D
+            alpha(d) = gamrnd(aa,1/ba(d));
+        end
+        
+        % update Theta
+        Theta_all(:,g)  = [beta; alpha];
+    end
 end
 stats.time_taken       = toc;
 
